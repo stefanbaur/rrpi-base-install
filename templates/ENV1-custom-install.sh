e35d0b0972ef805c4b0a1b74aa6ffb32a04d2bb1
Stefan Baur <X2Go-ML-1@baur-itcs.de>
2025-12-19 13:55:37 +0100

    removing templates for main branch

--- templates/ENV1-custom-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-# THIS SCRIPT WILL BE EXECUTED INSIDE THE CHANGEROOT, NO NEED TO CALL chroot HERE
-# run this right before umounting, so there's no apt clean call destroying our work
-# download the packages required for overlayroot, so you don't need internet access to activate it
-echo "Downloading required packages for overlayroot, so it can be installed later - even without internet access."
-apt-get install -d -y cryptsetup cryptsetup-bin overlayroot
