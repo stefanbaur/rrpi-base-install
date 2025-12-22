0a3100beadfca5aac3593e50b88a6cedeb91dbe3
Stefan Baur <X2Go-ML-1@baur-itcs.de>
2025-12-22 11:02:18 +0100

    new package names for trixie, no linphone-desktop

--- templates/ENV3-custom-install.sh
+++ templates/ENV3-custom-install.sh
@@ -2,4 +2,4 @@
 
 # THIS SCRIPT WILL BE EXECUTED INSIDE THE CHANGEROOT, NO NEED TO CALL chroot HERE
 echo "Downloading the required packages into ENV3, so they can be installed later - even without internet access."
-apt-get install -d -y openbox tint2 x11-xserver-utils feh x11vnc onboard network-manager-gnome libcamera-tools linphone-desktop baresip-core chromium-browser xdotool coreutils eatmydata xbindkeys alsa-utils pulseaudio-utils espeak pulseaudio
+apt-get install -d -y openbox tint2 x11-xserver-utils feh x11vnc onboard network-manager-gnome libcamera-tools linphone-cli baresip-core chromium chromium-sandbox chromium-l10n xdotool coreutils eatmydata xbindkeys alsa-utils pulseaudio-utils espeak pulseaudio
