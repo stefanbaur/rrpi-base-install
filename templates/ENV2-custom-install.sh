38e0db76651b9a36d7b3939381f5434b121eaf6e
Stefan Baur <X2Go-ML-1@baur-itcs.de>
2026-07-02 09:35:58 +0200

    when trying to install input drivers for X11/xorg, make sure the packages are downloaded beforehand

--- templates/ENV2-custom-install.sh
+++ templates/ENV2-custom-install.sh
@@ -2,4 +2,4 @@
 
 # THIS SCRIPT WILL BE EXECUTED INSIDE THE CHANGEROOT, NO NEED TO CALL chroot HERE
 echo "Downloading the required packages into ENV2, so they can be installed later - even without internet access."
-apt-get install -d -y xserver-xorg-video-fbdev openbox tint2 x11-xserver-utils x11vnc network-manager-gnome xdotool coreutils eatmydata xbindkeys alsa-utils pulseaudio-utils pulseaudio chromium chromium-l10n chromium-sandbox xterm x2goclient x2gokdriveclient nodm
+apt-get install -d -y xserver-xorg-video-fbdev xserver-xorg-input-libinput openbox tint2 x11-xserver-utils x11vnc network-manager-gnome xdotool coreutils eatmydata xbindkeys alsa-utils pulseaudio-utils pulseaudio chromium chromium-l10n chromium-sandbox xterm x2goclient x2gokdriveclient nodm
